754d9dd5906663ec6a3bfe0eee9c580ecf9ba9e1
Allal Mohamed Lamine <allalmohmaedlamine@gmail.com>
2021-06-25 11:01:46 +0100

    refactor: use Map

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,35 @@
-type HooksObj = { [name in any]: Function[] };
-
 export class Hooks<HooksDefinition extends { [name: string]: any }> {
-    private _hooks: HooksObj;
+    private _hooks: Map<
+        keyof HooksDefinition,
+        HooksDefinition[keyof HooksDefinition][]
+    >;
 
     constructor() {
-        this._hooks = {};
+        this._hooks = new Map();
     }
 
     public on<HookName extends keyof HooksDefinition>(
         hookName: HookName,
         callback: HooksDefinition[HookName]
     ) {
-        if (!this._hooks[hookName]) {
-            (this._hooks as any)[hookName] = [];
+        if (!this._hooks.has(hookName)) {
+            this._hooks.set(hookName, []);
         }
 
-        this._hooks[hookName].push(callback);
+        this._hooks.get(hookName).push(callback);
     }
 
     public unsubscribe<HookName extends keyof HooksDefinition>(
         hookName: HookName,
         callback: HooksDefinition[HookName]
     ): void {
-        if (this._hooks[hookName] && this._hooks[hookName].length > 0) {
-            const callbackIndex = this._hooks[hookName].indexOf(callback);
+        const hookList = this._hooks.get(hookName);
+
+        if (hookList && hookList.length > 0) {
+            const callbackIndex = hookList.indexOf(callback);
 
             if (callbackIndex > -1) {
-                this._hooks[hookName].splice(callbackIndex, 1);
+                hookList.splice(callbackIndex, 1);
                 // TODO: may be add key remove
             }
         }
@@ -37,7 +40,8 @@ export class Hooks<HooksDefinition extends { [name: string]: any }> {
         _this: any,
         ...args: Parameters<HooksDefinition[HookName]>
     ): void {
-        const hookCallbacks = this._hooks[hookName];
+        const hookCallbacks = this._hooks.get(hookName);
+
         if (hookCallbacks) {
             for (let i = 0; i < hookCallbacks.length; i++) {
                 hookCallbacks[i].call(_this, ...args);
@@ -50,7 +54,7 @@ export class Hooks<HooksDefinition extends { [name: string]: any }> {
         _this: any,
         ...args: Parameters<HooksDefinition[HookName]>
     ): Promise<any>[] {
-        const hookCallbacks = this._hooks[hookName];
+        const hookCallbacks = this._hooks.get(hookName);
         const promises: Promise<any>[] = [];
         if (hookCallbacks) {
             for (let i = 0; i < hookCallbacks.length; i++) {
